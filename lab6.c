1070967a498aa0f31b4053defabcfb594031d890
bjw3966 <bjw3966@naver.com>
2017-12-29 14:42:19 +0900

    lab6

---
+++ lab6.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <time.h>
+#include <fcntl.h>
+
+FILE *fp;
+int totaltime = 0;
+int main()
+{
+	char a[10] = "/proc/";
+	char id[10];
+	char c1[20], c2[20];
+	int pid;
+	int i;
+	char trash[100] = {0,};
+	int time;
+
+	pid=fork();
+
+	if(pid == 0)
+	{
+		printf("input processor id : ");
+		scanf("%s",id);
+
+		strcat(a,id);
+		strcpy(c1,a);
+		strcat(c1,"/stat");
+	
+		strcpy(c2,a);
+		strcat(c2,"/status");
+
+		fp = fopen(c1,"r");
+
+		for(i=0; i<13; i++)
+		{
+			fscanf(fp,"%s ",trash);
+		}
+
+		for(i=0; i<2; i++)
+		{
+			fscanf(fp,"%d ",&time);
+			printf("%d\n",time);
+			totaltime += time;
+		}
+
+		printf("time : %d\n",totaltime * 100);
+		fclose(fp);
+
+		fp = fopen(c2,"r");
+
+		while(!feof(fp))
+		{
+			fscanf(fp,"%s ",trash);
+
+			if(strcmp(trash,"VmSize:") == 0)
+			{
+				fscanf(fp,"%s",trash);
+				break;
+			}
+		}
+
+		printf("%s",trash);
+
+		fclose(fp);
+
+
+	}
+	else
+		wait();
+
+}
